85567557770cf70c642b509ce06489d620351280
Sam AntonY <saman2ny@outlook.com>
2021-04-11 01:41:14 +0530

    company val

--- src/pages/admin/company/company.component.ts
+++ src/pages/admin/company/company.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
+
+import { ApiService } from 'src/service/api.service';
+import { CommonService } from 'src/service/common.service';
+
+import { ConstantsService } from 'src/service/constants.service';
+
+declare var $: any;
 declare function datatblesandIts(): any;
 
 
@@ -8,11 +19,42 @@ declare function datatblesandIts(): any;
   styleUrls: ['./company.component.css']
 })
 export class CompanyComponent implements OnInit {
-
-  constructor() { }
+  CompanyForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private router: Router, private http: HttpClient, private route: ActivatedRoute, public common: CommonService, private apiService: ApiService,
+		public constantsService: ConstantsService, private location: Location
+	) { }
 
   ngOnInit(): void {
     datatblesandIts();
+
+        
+    this.CompanyForm = this.formBuilder.group({
+			companyName: ['', Validators.compose([Validators.required])],
+			contactPerson: ['', Validators.required],
+			address: ['', Validators.required],
+			country: ['', Validators.required],
+			city: ['', Validators.required],
+			state: ['', Validators.required],
+			postalCode: ['', Validators.required],
+			email: ['', Validators.required],
+			phoneNumber: ['', Validators.required],
+			mobileNumber: ['', Validators.required],
+			numberOfUsers: ['', Validators.required],
+			password: ['', Validators.required],
+			confirmPassword: ['', Validators.required],
+			website: [''],
+		});
+  }
+
+
+  Save(){
+    if (this.CompanyForm.invalid) {
+			this.CompanyForm.markAllAsTouched();
+			return;
+		} else {
+      this.common.hideLoading()
+
+    }
   }
 
 }
