af8979c8f89cfbf07415f54bada764b997131c5b
Sam AntonY <saman2ny@outlook.com>
2021-04-11 21:47:16 +0530

    companie

--- src/pages/admin/local/local.component.ts
+++ src/pages/admin/local/local.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
+
+import { ApiService } from 'src/service/api.service';
+import { CommonService } from 'src/service/common.service';
+
+import { ConstantsService } from 'src/service/constants.service';
+
+declare var $: any;
+
 declare function datatblesandIts(): any;
 
 
@@ -8,11 +20,32 @@ declare function datatblesandIts(): any;
   styleUrls: ['./local.component.css']
 })
 export class LocalComponent implements OnInit {
-
-  constructor() { }
+  LocalForm: FormGroup;
+  localObj:any = {}
+  constructor(private formBuilder: FormBuilder, private router: Router, private http: HttpClient, private route: ActivatedRoute, public common: CommonService, private apiService: ApiService,
+		public constantsService: ConstantsService, private location: Location
+	) { }
 
   ngOnInit(): void {
     datatblesandIts();
+
+    this.LocalForm = this.formBuilder.group({
+			companyName: ['', Validators.compose([Validators.required])],
+			contactPerson: ['', Validators.required],
+			address: ['', Validators.required],
+			country: ['', Validators.required],
+			city: ['', Validators.required],
+			state: ['', Validators.required],
+			postalCode: ['', Validators.required],
+			email: ['', Validators.required],
+			phoneNumber: ['', Validators.required],
+			mobileNumber: ['', Validators.required],
+			numberOfUsers: ['', Validators.required],
+			password: ['', Validators.required],
+			confirmPassword: ['', Validators.required],
+			website: [''],
+		});
+
   }
 
 }
