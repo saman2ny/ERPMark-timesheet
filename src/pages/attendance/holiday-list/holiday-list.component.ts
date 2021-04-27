748d8f50771a57582643af08e6850d88dbda265f
Sam AntonY <saman2ny@outlook.com>
2021-04-27 17:40:14 +0530

    holiday demo

--- src/pages/attendance/holiday-list/holiday-list.component.ts
+++ src/pages/attendance/holiday-list/holiday-list.component.ts
@@ -19,7 +19,7 @@ declare function datatblesandIts(): any;
   styleUrls: ['./holiday-list.component.css']
 })
 export class HolidayListComponent implements OnInit {
-
+  holidayList:  any = []
   
   constructor(private formBuilder: FormBuilder, private router: Router, private http: HttpClient, private route: ActivatedRoute, public common: CommonService, private apiService: ApiService,
 		public constantsService: ConstantsService, private location: Location
@@ -27,7 +27,16 @@ export class HolidayListComponent implements OnInit {
 
   ngOnInit(): void {
     datatblesandIts();
+    this.initalLoadUp()
 
+  }
+  initalLoadUp(){
+    this.holidayList = [
+      {id: "1", name:"sam", date:"sam", time:"sam", status:"sam"},
+      {id: "2", name:"antony", date:"sam", time:"sam", status:"sam"},
+      {id: "3", name:"teminos", date:"sam", time:"sam", status:"sam"},
+      {id: "4", name:"joseph",date:"sam", time:"sam", status:"sam"},
+    ]
   }
 
 }
